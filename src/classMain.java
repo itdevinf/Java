9d3f33e3d09f623ae40b5775aba7be4515c0f45e
frup44339 <frup44339@ITMFR5CG2432FG9.annuaire.groupe.local>
2026-07-03 16:12:26 +0200

    Test github

---
+++ src/classMain.java
@@ -0,0 +1,14 @@
+
+public class classMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("classMain");
+		System.out.println("1/ Créer un dépot local");
+		System.out.println("1.1/ Sélectionner le projet myJavaGitProjects");
+		System.out.println("1.2/ Menu window->Perspective->Open Perspective->Other...->Git->Open");
+		System.out.println("eclipse-workspace/myJavaGitProjects");
+		
+	}
+
+}
