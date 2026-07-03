08ac06bf68b5e07626b609f3d55a8556008d0783
frup44339 <frup44339@ITMFR5CG2432FG9.annuaire.groupe.local>
2026-07-03 22:15:55 +0200

    Mise à jour explain how to connect with token

--- src/classMain.java
+++ src/classMain.java
@@ -4,9 +4,19 @@ public class classMain {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("classMain");
-		System.out.println("1/ Créer un dépot local");
-		System.out.println("1.1/ Sélectionner le projet myJavaGitProjects");
-		System.out.println("1.2/ Menu window->Perspective->Open Perspective->Other...->Git->Open");
+		System.out.println("1/ Créer un token github");
+		System.out.println("1.1/ Se connecter à github>Profil->Setup");
+		System.out.println("1.2/ Colonne de gauche->Personnal access tokens->Tokens(classic)");
+		System.out.println("1.3/ Colonne de droite->Generate new token(classic)");
+		System.out.println("1.4/ Note:TestToken");
+		System.out.println("1.5/ Expiration:No expiration");
+		System.out.println("1.6/ Sélect scopes:repo");
+		System.out.println("1.7/ Generate token");
+		System.out.println("1.7/ Copier le token généré");
+
+		System.out.println("2/ Créer un dépot local");
+		System.out.println("2.1/ Sélectionner le projet myJavaGitProjects");
+		System.out.println("2.2/ Menu window->Perspective->Open Perspective->Other...->Git->Open");
 		System.out.println("eclipse-workspace/myJavaGitProjects");
 		
 	}
